e974b6d47283c3474e635c4364a29e11173f4624
Andrew Spott <andrew.spott@gmail.com>
2015-05-27 16:50:14 -0600

    new version without depreciation warnings

--- ipynb_output_filter.py
+++ ipynb_output_filter.py
@@ -1,18 +1,34 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 
+"""
+Usage: python remove_output.py notebook.ipynb [ > without_output.ipynb ]
+
+Modified from remove_output by:
+damianavila (https://gist.github.com/damianavila/5305869)
+"""
 import sys
-from IPython.nbformat.current import read, write
+from IPython.nbformat import read, write
 
-json_in = read(sys.stdin, 'json')
 
-for sheet in json_in.worksheets:
-    for cell in sheet.cells:
-        if "outputs" in cell:
+def remove_outputs(nb):
+    """remove the outputs from a notebook"""
+    for cell in nb.cells:
+        if cell.cell_type == 'code':
             cell.outputs = []
-        if "prompt_number" in cell:
-            del cell["prompt_number"]
+            cell.execution_count = None
+    # for sheet in nb.worksheets:
+    #     for cell in sheet.cells:
+    #         if "outputs" in cell:
+    #             cell.outputs = []
+    #         if "prompt_number" in cell:
+    #             del cell["prompt_number"]
+    #         if "execution_count" in cell:
+    #             del cell["execution_count"]
+    # if 'signature' in nb.metadata:
+    #     nb.metadata['signature'] = ""
 
-if 'signature' in json_in.metadata:
-    json_in.metadata['signature'] = ""
+if __name__ == '__main__':
+    nb = read(sys.stdin, 4)
+    remove_outputs(nb)
+    write(nb, sys.stdout, 4)
 
-write(json_in, sys.stdout, 'json')
